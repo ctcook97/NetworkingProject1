58e5580ba3e4a53273eeaeaefe0cdea2a136b91b
ctcook97 <ctcook97@gmail.com>
2019-02-13 19:34:04 -0500

    Deleted extra files

--- server.java
+++ server.java
@@ -17,13 +17,23 @@ public class Server {
 
         while(true){
             try (
+                //Set up server socket
                 ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
                 Socket clientSocket = serverSocket.accept();     
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                //Set up file writer
+                FileWriter fileWriter = new FileWriter("qbank.5"); //should close later
+
+
+
             ) {
                 String inputLine;
                 while ((inputLine = in.readLine()) != null) {
+                    
+                    fileWriter.write(inputLine);
+
                     out.println(inputLine);
                 }
             } catch (IOException e) {
@@ -31,6 +41,6 @@ public class Server {
                 System.out.println(e.getMessage());
             }
         }
-        
+
     }
 }
